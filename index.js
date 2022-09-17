df44681a6ba975eeb29eb6cd1751055df3b32744
Aryaman Gupta <aryamangupta2002@gmail.com>
2022-09-17 06:50:13 -0400

    Remove unnecessary top input box

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+let messages =[]
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-
-var clients = [];
 
 server.listen(3000, () => {
     console.log("Listening on *:3000");
 });
 
 io.on('connection', (socket) => {
-    clients.push(socket.clientid);
     console.log("User connected");
 
     socket.on('send message', (msg) => {
-        console.log('message: ' + msg);
+        console.log("Recieved message:" + msg);
         io.emit('send message', msg);
     });
 });
