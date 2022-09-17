4d88a66f1d86b230e62da4c91e22734b9eae57b1
navdeep03gill <navdeep03gill@gmail.com>
2022-09-17 18:38:11 -0400

    indexhtml updated

--- gameManager.js
+++ gameManager.js
@@ -1,10 +1,25 @@
+const justinurl = 'https://htn-backend-jtoft.k8s.csclub.cloud/getRandomWord';
+
 var messages = document.getElementById('messages');
 var input = document.getElementById('input');
 
 var timer; 
+
 var timeLeft = 11; // seconds
 
 var currAnswer;
+
+/*
+const sendHTTPRequest = (method, url, data) => {
+  return fetch(url)
+  .then(response => {
+    return response.json();
+  })
+  .then(responseData => {
+    console.log(responseData);
+  });
+};
+*/
 
 function getInput(){
   if (input.value) {
@@ -22,6 +37,7 @@ function gameOver() {
   clearInterval(timer);
   input.style.visibility = "hidden";
   showAnswer();
+  getWord();
 }
 
 function showAnswer(){
@@ -33,8 +49,7 @@ function showAnswer(){
 
 function updateTimer() {
   timeLeft = timeLeft - 1;
-  if(timeLeft >= 0)
-    $('#timer').html(timeLeft);
+  if (timeLeft >= 0) $("#timer").html(timeLeft);
   else {
     gameOver();
   }
@@ -45,13 +60,33 @@ function start() {
   // setInterval is a built-in function that will call the given function
   // every N milliseconds (1 second = 1000 ms)
   timer = setInterval(updateTimer, 1000);
-  
+
   // It will be a whole second before the time changes, so we'll call the update
   // once ourselves
-  $('#playAgainButton').hide();
+  $("#playAgainButton").hide();
 
   updateTimer();
-  
+
+  var socket = io();
+  var messages = document.getElementById("messages");
+  var form = document.getElementById("form");
+  var input = document.getElementById("input");
+
+  form.addEventListener("submit", function (e) {
+    //e.preventDefault();
+    if (input.value) {
+      socket.emit("send message", input.value);
+      input.value = "";
+    }
+  });
+
+  socket.on("send message", (msg) => {
+    var item = document.createElement("li");
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
   // We don't want the to be able to restart the timer while it is running,
   // so hide the button.
 }
